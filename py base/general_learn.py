9e26205946492d0b9013cbcbcf7b6243dcb4e430
elcaskerito <idimhtt4t@gmail.com>
2025-04-23 03:08:00 -0400

    code updated with insertion of bar chart to display importance factor

---
+++ py base/general_learn.py
@@ -0,0 +1,23 @@
+# print a simple word
+
+print( ' Hello word! ')
+
+# conditonal instruction (if - else)
+
+love = 1
+if love <1 :
+    print('no love, heart break ')
+else :
+    print(' I am in love')
+
+# loop while (while {condition})
+
+iteration =1
+while iteration >0:
+     print("we are going to infinite")
+     if iteration == 10 :
+         iteration = 0
+     else :
+         iteration +=1
+
+     
